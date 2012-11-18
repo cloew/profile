ccebffd1cd5cea736e55948be8dafc13e04e3944
Chris Loew <cloew123@gmail.com>
2012-11-18 16:20:59 -0700

    Added prepare.sh to setup the standard directory structure

---
+++ .prepare.sh
@@ -0,0 +1,46 @@
+#! /usr/bin/bash
+
+# Source the Bash Profile
+if [ -f ~/.bashrc ]; then
+	source ~/.bashrc
+fi
+
+# Set Git Default 
+cd ~
+gpsom
+
+# Set up core folders
+mkdir $dev_dir
+mkdir $ide_dir
+mdkir $dev_dir/Test
+
+# Setup Kao Tessur Folders
+mkdir $kao_dir
+
+# Get PIP
+get-pip
+pip install blessings
+
+# Setup Kao Console
+mkdir $kao_console_dir
+cd $kao_console_dir
+g-start git@github.com:cloew/KaoConsole.git
+py-install # Install the Kao Console
+
+# Setup Nyt Owl
+mkdir $nytowl_dir
+cd $nytowl_dir
+g-start git@github.com:cloew/Nyt-Owl-Editor.git
+
+# Setup Pokemon Python
+mkdir $pkmn_dir
+cd $pkmn_dir
+g-start git@github.com:cloew/Pokemon-Project.git
+
+# Setup Excelcient Directories
+mkdir $exc_dir
+
+# Setup Prosperion
+mkdir $prosp_dir
+cd $prosp_dir
+g-start git@github.com:cloew/Project001.git
