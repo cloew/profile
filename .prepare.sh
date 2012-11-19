10b27f2b6c202b3215d1abf029ff9edf2a29be2a
Chris Loew <foediddly@Firefly.(none)>
2012-11-18 19:32:14 -0700

    Cleaned Prepare.sh so it actually works!

--- .prepare.sh
+++ .prepare.sh
@@ -1,18 +1,22 @@
 #! /usr/bin/bash
 
-# Source the Bash Profile
-if [ -f ~/.bashrc ]; then
-	source ~/.bashrc
-fi
+function mkgitdir () {
+    mkdir $1
+    cd $1
+    git init
+    git remote add origin $2
+    git pull origin master
+    git push -u origin master
+}
 
 # Set Git Default 
 cd ~
-gpsom
+git push -u origin master
 
 # Set up core folders
 mkdir $dev_dir
 mkdir $ide_dir
-mdkir $test_dir
+mkdir $test_dir
 
 # Setup Kao Tessur Folders
 mkdir $kao_dir
@@ -22,25 +26,17 @@ get-pip
 pip install blessings
 
 # Setup Kao Console
-mkdir $kao_console_dir
-cd $kao_console_dir
-g-start git@github.com:cloew/KaoConsole.git
-py-install # Install the Kao Console
+mkgitdir $kao_console_dir git@github.com:cloew/KaoConsole.git
+python setup.py install # Install the Kao Console
 
 # Setup Nyt Owl
-mkdir $nytowl_dir
-cd $nytowl_dir
-g-start git@github.com:cloew/Nyt-Owl-Editor.git
+mkgitdir $nytowl_dir git@github.com:cloew/Nyt-Owl-Editor.git
 
 # Setup Pokemon Python
-mkdir $pkmn_dir
-cd $pkmn_dir
-g-start git@github.com:cloew/Pokemon-Project.git
+mkgitdir $pkmn_dir git@github.com:cloew/Pokemon-Project.git
 
 # Setup Excelcient Directories
 mkdir $exc_dir
 
 # Setup Prosperion
-mkdir $prosp_dir
-cd $prosp_dir
-g-start git@github.com:cloew/Project001.git
+mkgitdir $prosp_dir git@github.com:cloew/Project001.git
