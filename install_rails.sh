9f4ba109f6d22a905ba167f354904c852ef4265a
Chris Loew <cloew123@gmail.com>
2012-11-18 21:45:12 -0700

    Added g++ to the install rails requirements

--- install_rails.sh
+++ install_rails.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/bash
 
-alias install="sudo apt-get install"
+install="sudo apt-get install"
 
 sudo apt-get update
-install libsqlite3-dev
-install rub1.9.1-dev
+$install g++
+$install libsqlite3-dev
+$install ruby1.9.1-dev
 sudo gem install rails
 sudo gem install sqlite3
